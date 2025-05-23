f5f3fb95c1e1a4105525d33ca8e17d99182b11a3
Yunus <38208@ma-web.nl>
2025-05-23 10:47:58 +0200

    les1

---
+++ React-pagina/src/AboutMe.jsx
@@ -0,0 +1,15 @@
+export function AboutMe(){
+    return(
+       <article className="AboutMe">
+        <img src="img/Goku.png" alt="foto van goku" />
+        <div>
+            <h2 className="AboutMe_title">About me</h2>
+            <p className="AboutMe_paragraph">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Recusandae alias laborum, suscipit labore, 
+                cum nihil vero officiis harum rerum perferendis debitis adipisci cumque quod repellat expedita vel illo soluta unde!
+            </p>
+        </div>
+       </article>
+    )
+}
---
+++ React-pagina/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'   
+import './AboutMe.css'
+import { AboutMe } from './AboutMe';
+
+function App() {
+  return (
+  <main>
+      <AboutMe/>
+      <hr/>
+  </main>  
+
+  
+  )
+}
+
+export default App
+ 
